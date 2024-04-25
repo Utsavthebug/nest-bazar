68c988168dfa6628038ea4435249ad26d8c23745
Utsavthebug <neupaneutsav.2001@gmail.com>
2024-04-25 17:26:35 +0545

    authentication guard added

--- src/role/guards/permission.guard.ts
+++ src/role/guards/permission.guard.ts
@@ -5,22 +5,27 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+    private readonly roleService: RoleService,
+    private reflector: Reflector
+  ) {}
 
-  canActivate(
-    context: ExecutionContext
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return new Promise(async (resolve) => {
-      const neededPermission = this.reflector.getAllAndOverride<string[]>(
-        PERMISSION_KEY,
-        [context.getHandler(), context.getClass()]
-      );
-      if (!neededPermission) {
-        return true;
-      }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const neededPermission = this.reflector.getAllAndOverride<string[]>(
+      PERMISSION_KEY,
+      [context.getHandler(), context.getClass()]
+    );
+    if (!neededPermission) {
+      return true;
+    }
 
-      const { user } = context.switchToHttp().getRequest();
-      return neededPermission.some((role) => user.roles?.includes(role));
-    });
+    const req = context.switchToHttp().getRequest();
+    const id = req['user'];
+
+    try {
+      const { role } = await this.userService.findOne(id);
+    } catch (error) {}
   }
 }
